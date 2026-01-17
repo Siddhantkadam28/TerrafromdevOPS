2e9d7b5780c5bc5b0e620ff0d0aac184dd8e64e9
Siddhantkadam28 <113542239+Siddhantkadam28@users.noreply.github.com>
2026-01-17 12:16:15 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "my-terraform-state-bucket"
-    key            = "jenkins-project1/terraform.tfstate"
+    key            = "project1/terraform.tfstate"
     region         = "ap-south-1"
     encrypt        = true
     dynamodb_table = "terraform-lock"
