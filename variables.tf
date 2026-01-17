b0fbcf74b42d73554122ad2079e749a04b9e4200
Siddhantkadam28 <113542239+Siddhantkadam28@users.noreply.github.com>
2026-01-17 12:14:01 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "aws_region" {
+  default = "ap-south-1"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "ami_id" {
+  description = "Amazon Linux 2 AMI"
+  default     = "ami-0f58b397bc5c1f2e8"
+}
